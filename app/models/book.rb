3b4f7826a2fdbe5a5a4e8ff37d978e8fe1c09d50
kazuto ando <kazuto.ando@gmail.com>
2025-01-26 13:27:04 +0900

    add model scope

---
+++ app/models/book.rb
@@ -0,0 +1,4 @@
+class Book < ApplicationRecord
+  scope :costly, -> { where("price > ?", 3000) }
+  scope :written_about, ->(them) {where("name like ?", "%#{them}%")}
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_26_030340) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_26_040040) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
