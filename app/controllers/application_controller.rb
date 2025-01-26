e4308021506e3d585480732b920c63e27474b31e
kazuto ando <kazuto.ando@gmail.com>
2025-01-26 19:50:09 +0900

    add database status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :require_login
+
+  def require_login
+    # ログインを確認し、非ログイン時にリダイレクトする処理
+  end
+
+  before_action do
+    redirect_to access_denied_path if params[:token].blank?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,14 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destroy"
+
+  resources :publishers
+
   # Defines the root path route ("/")
   root "articles#index"
+
 
   resources :articles
   # get "/articles", to: "articles#index"
