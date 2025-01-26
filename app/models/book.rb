b31ae01a43272374e8fc24b129361cad964cd5e5
kazuto ando <kazuto.ando@gmail.com>
2025-01-26 17:52:59 +0900

    add before_validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,12 +14,29 @@ class Book < ApplicationRecord
   validates :name, :price, presence: true
   validates :name, length: { maximum: 25 }
   validates :price, numericality: { greater_than_or_equal_to:  0 }
+
+  before_validation do
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+
+  # あるいはメソッドを使って以下のようにも書ける
+  # before_validation :add_lovely_to_cat
+  #
+  # def add_lovely_to_cat
+  #   self.name = self.name.gsub(/Cat/) do |matched|
+  #     "lovely #{matched}"
+  #   end
+  # end
+
   # 独自バリデーション
   validate do |book|
     if book.name.include?("exercise")
       book.errors[:name] << " I dont like exercise."
     end
   end
+
 end
 
 
