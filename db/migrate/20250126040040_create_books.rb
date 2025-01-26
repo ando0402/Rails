c04ba47c75f9b2b7514b7266c11a8eb3dfc8e9cb
kazuto ando <kazuto.ando@gmail.com>
2025-01-26 14:09:52 +0900

    change migrate

--- db/migrate/20250126040040_create_books.rb
+++ db/migrate/20250126040040_create_books.rb
@@ -1,6 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[7.2]
   def change
     create_table :books do |t|
+      t.string :name
+      t.datetime :price
+      t.datetime :published_on
       t.timestamps
     end
   end
