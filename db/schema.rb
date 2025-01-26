25811255889a100ed29f86941fa41b4c30c09dae
kazuto ando <kazuto.ando@gmail.com>
2025-01-26 15:06:40 +0900

    change schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_26_040040) do
   end
 
   create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.date "published_on"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
