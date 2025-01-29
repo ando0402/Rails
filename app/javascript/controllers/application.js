38c351e2d28df1d10ccb2877e900bb6f31a6b989
kazuto ando <kazuto.ando@gmail.com>
2025-01-30 08:43:04 +0900

    assets:precompile

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,16 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// slide_panel
+import './stylesheets/slide_panel.css';
+
+// Monaco Editor
+import * as monaco from 'monaco-editor';
+
+window.onload = function() {
+    const editor = monaco.editor.create(document.getElementById('container'), {
+        value: "function hello() {\n\talert('Hello, world!');\n}",
+        language: "javascript"
+    });
+};
