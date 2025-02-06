ebcab11ffea311ff6adf6a94e8042085ee4b513a
kazuto ando <kazuto.ando@gmail.com>
2025-02-06 09:02:17 +0900

    active job queut

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,6 +1,18 @@
 class AsyncLogJob < ApplicationJob
   # queue_as :default
   queue_as :async_log
+
+  # 渡されたメッセージ内容によって追加するキュー決める
+  queue_as do
+    case self.arguments.first[:message]
+    when "to async_log"
+      :async_log
+    else
+      :default
+    end
+    end
+  end
+
   def perform(message: "hello") # message引数を追加
     # Do something later
     AsyncLog.create!(message: message) # DBに保存する
