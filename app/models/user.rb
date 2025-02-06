d7af14802e060b9556920c911498e0232d2f0e56
kazuto ando <kazuto.ando@gmail.com>
2025-02-06 15:07:22 +0900

    add thumb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_one_attached :portrait
+  # has_one_attached :portrait
   # thumb
-  # has_one_attached :portrait do |attachable|
-  #   attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
-  # end
+  has_one_attached :portrait do |attachable|
+    attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
+  end
 end
