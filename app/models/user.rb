56ff858ad71eec4bba270118541ea5dd06d17b52
kazuto ando <kazuto.ando@gmail.com>
2025-02-06 16:57:52 +0900

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  # has_one_attached :portrait
+  validates :username, presence: true
+  has_one_attached :portrait
   # thumb
   has_one_attached :portrait do |attachable|
     attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
