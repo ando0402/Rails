001b428c25cf4f21499761c53b222a3f868580e6
kazuto ando <kazuto.ando@gmail.com>
2025-02-10 16:34:11 +0900

    add room js

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,26 +1,25 @@
 import consumer from "./consumer"
 // クライアントサイド
-consumer.subscriptions.create("RoomChannel", {
-// window.App = consumer.subscriptions.create("RoomChannel", {
-    // 接続
-    connected() {
-      // Called when the subscription is ready for use on the server
-    },
+// consumer.subscriptions.create("RoomChannel", {
+window.App = consumer.subscriptions.create("RoomChannel", {
+  // 接続
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
 
-    // 切断時
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
+  // 切断時
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
 
-    // 受信
-    received(data) {
-      // Called when there's incoming data on the websocket for this channel
-      alert(data["message"]);
-    },
+  // 受信
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    alert(data["message"]);
+  },
 
-    speak: function(message) {
-      // return this.perform('speak');
-      return this.perform('speak', ["message", message]);
-    }
-  })
-};
+  speak: function(message) {
+    // return this.perform('speak');
+  return this.perform('speak', ["message", message]);
+  }
+});
