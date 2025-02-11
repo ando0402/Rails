550e3c6abbe1123fa0aa6f5e03826ad89de09a50
kazuto ando <kazuto.ando@gmail.com>
2025-02-11 19:48:22 +0900

    add subscribes

--- test/channels/room_channel_test.rb
+++ test/channels/room_channel_test.rb
@@ -1,6 +1,21 @@
 require "test_helper"
 
 class RoomChannelTest < ActionCable::Channel::TestCase
+
+  test "subscribes" do
+    subscribe
+    text = "Hello"
+    broadcast_text = ApplicationController.render(
+      partial: "messages/message",
+      locals: { message: Message.new(content:  text) }
+    )
+
+    assert_broadcast_on("room_channel", message: broadcast_text) do
+      perform :speak, message: text
+    end
+
+  end
+
   # test "subscribes" do
   #   subscribe
   #   assert subscription.confirmed?
