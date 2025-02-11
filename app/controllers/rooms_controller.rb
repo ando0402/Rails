9cadabefaab393dcdd16c613238c9d1382ceaee7
kazuto ando <kazuto.ando@gmail.com>
2025-02-11 18:11:17 +0900

    add room login

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,11 @@
 class RoomsController < ApplicationController
+  # def show
+  #   @messages = Message.all
+  # end
   def show
+    # ログイン機能の仮コード
+    cookies.signed[:user_id] = User.first.id
     @messages = Message.all
   end
+
 end
