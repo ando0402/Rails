65ac9b941964f15d523ee9e1bb5042c20554636d
kazuto ando <kazuto.ando@gmail.com>
2025-02-11 19:36:33 +0900

    add subscribe

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 
 module ApplicationCable
   class ConnectionTest < ActionCable::Connection::TestCase
+    test "subscribes" do
+      subscribe
+      assert subscription.confirmed?
+      assert_has_stream "room_channel"
+    end
     # test "connects with cookies" do
     #   cookies.signed[:user_id] = 42
     #
