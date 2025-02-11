6068e0ff9789779a766b376d78bc923044c71ef5
kazuto ando <kazuto.ando@gmail.com>
2025-02-11 19:57:10 +0900

    add connection

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -2,11 +2,20 @@ require "test_helper"
 
 module ApplicationCable
   class ConnectionTest < ActionCable::Connection::TestCase
-    test "subscribes" do
-      subscribe
-      assert subscription.confirmed?
-      assert_has_stream "room_channel"
+    # test "subscribes" do
+    #   subscribe
+    #   assert subscription.confirmed?
+    #   assert_has_stream "room_channel"
+    # end
+
+    test 'connection accepts' do
+      user = User.first
+      cookies.signed[:user_id] = User.first.id
+      connect
+      assert_equal connection.current_user.id, user.id
     end
+
+
     # test "connects with cookies" do
     #   cookies.signed[:user_id] = 42
     #
