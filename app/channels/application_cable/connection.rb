cee8ba750cf8e40afac983fcca423679db8b6cfd
kazuto ando <kazuto.ando@gmail.com>
2025-02-11 19:25:08 +0900

    add login process

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_venified_user
+    end
+
+    private
+
+    def find_venified_user
+      if verified_user = User.find_by(id: cookies.signed[:user_id])
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
