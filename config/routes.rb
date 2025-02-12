ce4e52d1c79ee921e3d5fc676c363123228fe7f7
kazuto ando <kazuto.ando@gmail.com>
2025-02-13 08:31:18 +0900

    add routes login

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
-  get "/auth/:provider/callback" => "sessions#create"
+  # get "/auth/:provider/callback" => "sessions#create"
+  get "auth/:provider/callback", to: "sessions#create"
+  get "/login", to: "sessions#new"
   delete "/logout" => "sessions#destroy"
 
   # リダイレクト処理
