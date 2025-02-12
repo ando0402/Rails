69409ab21f3506a98ba20783d0dbfc5ed195103d
kazuto ando <kazuto.ando@gmail.com>
2025-02-12 15:05:46 +0900

    add routes auth

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
+  get "/auth/:provider/callback" => "sessions#create"
 
   # リダイレクト処理
   # respond_to do | format |
