f0d55516de59331a4c0e7def136499a71df342c7
kazuto ando <kazuto.ando@gmail.com>
2025-02-12 18:23:41 +0900

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
+
+  # Login
   def create
     user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_path, notice: "ログインしました"
   end
+
+  # Logout
+  def destroy
+    reset_session
+    redirect_to root_path, notice: "ログアウトしました"
+  end
+
 end
