6ca2ce60a983bd2cd60934dabc92d8f4d46a10e6
kazuto ando <kazuto.ando@gmail.com>
2025-02-12 16:16:45 +0900

    add omniauth github Read from environment variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   if Rails.env.development? || Rails.env.test?
     # provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-    provider :github, "YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET"
+    # provider :github, "YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET"
+    provider :github, "Ov23ct6XQbYfOknbTWc5", "d1856bb6256d2d332ca946af4ff9d247396bc9ec"
   else
     provider :github,
      Rails.application.credentials.github[:client_id],
