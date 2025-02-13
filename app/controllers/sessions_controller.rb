29889ded5df7759a60f9b4a500742fdf5f49d088
kazuto ando <kazuto.ando@gmail.com>
2025-02-13 12:33:04 +0900

    add login writing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,11 @@ class SessionsController < ApplicationController
 
   # Login
   def create
-    user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_path, notice: "ログインしました"
+    ActiveRecord::Base.connected_to(role: :writing) do
+      user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "ログインしました"
+    end
   end
 
   # Logout
