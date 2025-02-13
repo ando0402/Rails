71031f04b52b6850d00357c03be148d3e9b81951
kazuto ando <kazuto.ando@gmail.com>
2025-02-13 17:04:54 +0900

    add TimeZone

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Blog
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # config.active_record.writing_role = :default
