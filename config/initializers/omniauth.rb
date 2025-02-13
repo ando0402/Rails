e7511525d4489ff8348d3800e0ba16049e81f895
kazuto ando <kazuto.ando@gmail.com>
2025-02-13 11:52:27 +0900

    add OmniAuth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,10 +6,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     # provider :github, "YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET"
     provider :github,
              Rails.application.credentials.github[:client_id],
-             Rails.application.credentials.config[:client_secret]
+             Rails.application.credentials.github[:client_secret]
   else
     provider :github,
      Rails.application.credentials.github[:client_id],
-     Rails.application.credentials.config[:client_secret]
+     Rails.application.credentials.github[:client_secret]
   end
 end
